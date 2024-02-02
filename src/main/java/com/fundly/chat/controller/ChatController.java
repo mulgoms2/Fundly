bd360d2ce2613f135e5b346d10bde99c1f1ee8b3
yoonjae <mulgoms2@gmail.com>
2024-02-02 20:13:37 +0900

    add tomcat embeded websocket

--- src/main/java/com/fundly/chat/controller/ChatController.java
+++ src/main/java/com/fundly/chat/controller/ChatController.java
@@ -1,27 +1,30 @@
 package com.fundly.chat.controller;
 
-import com.fundly.chat.pojo.Greeting;
-import com.fundly.chat.pojo.HelloMessage;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.util.HtmlUtils;
 
 @Controller
 @Slf4j
-public class GreetingController {
+public class ChatController {
 
     @GetMapping("/chat")
     public String chat() {
         return "chat/chat";
     }
 
-    //    채팅 메시지를 불러온다
+//    //    채팅 메시지를 불러온다
+//    @MessageMapping("/hello")
+//    @SendTo("/topic/greetings")
+//    public Greeting greeting(HelloMessage message) {
+//        return new Greeting("hello, " + HtmlUtils.htmlEscape(message.getName()));
+//    }
+
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
-    public Greeting greeting(HelloMessage message) {
-        return new Greeting("hello, " + HtmlUtils.htmlEscape(message.getName()));
+    public String greeting(String message) {
+        return message;
     }
 }
--- src/test/java/com/fundly/user/model/userDaoImplTest.java
+++ src/test/java/com/fundly/user/model/userDaoImplTest.java
@@ -1,16 +1,16 @@
 package com.fundly.user.model;
 
-import config.*;
+import config.RootContext;
+import config.ServletContext;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Slf4j
 @SpringJUnitWebConfig(classes = {RootContext.class, ServletContext.class})
 class userDaoImplTest {
-
 
 
     @Test
