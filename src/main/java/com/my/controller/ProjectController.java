c3203432f2de6a99d940126ad09d50b28b776a86
pickylemon <novecento104@gmail.com>
2024-02-02 19:59:27 +0900

    backup

--- src/main/java/com/my/controller/ProjectController.java
+++ src/main/java/com/my/controller/ProjectController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
-@RequestMapping("/project")
-public class ProjectController {
-    @GetMapping("/project-editor")
-    public String projectInit(){
-        return "project.management";
-    }
-}
+//@Controller
+//@RequestMapping("/project")
+//public class ProjectController {
+//    @GetMapping("/project-editor")
+//    public String projectInit(){
+//        return "project.management";
+//    }
+//}
