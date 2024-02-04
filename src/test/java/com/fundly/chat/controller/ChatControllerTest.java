a0c85f50b5d34a0a2c3d8400ea32b7748999df92
mulgoms2 <77871865+mulgoms2@users.noreply.github.com>
2024-02-05 01:14:43 +0900

    Update ChatControllerTest.java

--- src/test/java/com/fundly/chat/controller/ChatControllerTest.java
+++ src/test/java/com/fundly/chat/controller/ChatControllerTest.java
@@ -1,36 +1,4 @@
-<<<<<<< HEAD:src/test/java/com/fundly/user/model/userDaoImplTest.java
-//
-//package com.fundly.user.model;
-//
-//import config.RootContext;
-//import config.ServletContext;
-//import lombok.SneakyThrows;
-//import lombok.extern.slf4j.Slf4j;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
-//
-//import static org.junit.jupiter.api.Assertions.assertTrue;
-//
-//@Slf4j
-//@SpringJUnitWebConfig(classes = {RootContext.class, ServletContext.class})
-//class userDaoImplTest {
-//
-//    @Autowired
-//    userDao userDao;
-//
-//    @Test
-//    @SneakyThrows
-//    void count() {
-//        userDao.count();
-//        assertTrue(true);
-//    }
-//
-//    @Test
-//    void insert() {
-//    }
-//}
-=======
+
 package com.fundly.chat.controller;
 
 import com.fundly.chat.model.ChatRoomDao;
@@ -60,4 +28,3 @@ class ChatControllerTest {
 
     }
 }
->>>>>>> ecafd709e797faacfbd4198e7c4d856eb914677b:src/test/java/com/fundly/chat/controller/ChatControllerTest.java
