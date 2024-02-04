d140d41de246d1bc0d1596f863ebd70b2790243a
yoonjae <mulgoms2@gmail.com>
2024-02-05 01:17:03 +0900

    Docs  mapper path

---
+++ src/main/java/com/persistence/dto/SelBuyMsgDetails.java
@@ -0,0 +1,23 @@
+package com.persistence.dto;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class SelBuyMsgDetails {
+
+//    private String msgId;
+    private String buyId;
+    private String pjId;
+    private Integer seq;
+    private String sendUserId;
+    private Date svrIntime;
+    private String msgReadYn;
+    private String msgCont;
+    private Date dbaRegDtm;
+    private String dbaRegId;
+    private Date dbaModDtm;
+    private String dbaModId;
+    private Integer fileCnt;
+}
