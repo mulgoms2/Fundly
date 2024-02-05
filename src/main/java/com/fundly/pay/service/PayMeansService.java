57309d4025191955de244cc0ce03c1f484afd214
howecofe <am0229@naver.com>
2024-02-05 18:10:12 +0900

    결제수단: Service 작성

---
+++ src/main/java/com/fundly/pay/service/PayMeansService.java
@@ -0,0 +1,21 @@
+package com.fundly.pay.service;
+
+import com.persistence.dto.PayMeansDto;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface PayMeansService {
+    int getPayMeansCount() throws Exception;
+    int registerPayMeans(PayMeansDto payMeansDto) throws Exception;
+    int unsetDefaultPayMeans(PayMeansDto payMeansDto) throws Exception;
+    int setDefaultPayMeans(PayMeansDto payMeansDto) throws Exception;
+    int removePayMeans(String pay_means_id) throws Exception;
+    int removeAllPayMeansForAdmin() throws Exception;
+    PayMeansDto getPayMeans(String pay_means_id) throws Exception;
+    List<PayMeansDto> getAllPayMeans(String user_id) throws Exception;
+    List<PayMeansDto> getAllPayMeansForAdmin() throws Exception;
+    PayMeansDto getDefaultPayMeans() throws Exception;
+
+}
