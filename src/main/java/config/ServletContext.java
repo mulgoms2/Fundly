440d3aa70a3564b7978b47685a37c89d0224382c
howecofe <am0229@naver.com>
2024-02-08 18:07:46 +0900

    common: WebClient 빈 등록

--- src/main/java/config/ServletContext.java
+++ src/main/java/config/ServletContext.java
@@ -1,9 +1,15 @@
 package config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
@@ -14,7 +20,12 @@ import org.springframework.web.servlet.view.tiles3.TilesView;
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = {"com", "com.fundly"})
+@PropertySource(value = "/WEB-INF/config/pay.properties")
 public class ServletContext implements WebMvcConfigurer {
+
+    @Autowired
+    Environment env;
+
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
         WebMvcConfigurer.super.configureViewResolvers(registry);
@@ -37,5 +48,14 @@ public class ServletContext implements WebMvcConfigurer {
     public CommonsMultipartResolver multipartResolver() {
         return new CommonsMultipartResolver();
 
+    }
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder()
+                .baseUrl(env.getProperty("PORTONE_BASE_URL"))
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+                .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+                .build();
     }
 }
