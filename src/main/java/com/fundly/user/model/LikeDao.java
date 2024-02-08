5c6fac02730166e36bc4937bf546604b8f4e8ee0
bada <silverzoo030@gmail.com>
2024-02-08 14:28:52 +0900

    🌱 Update: complete likedao test

--- src/main/java/com/fundly/user/model/LikeDao.java
+++ src/main/java/com/fundly/user/model/LikeDao.java
@@ -2,7 +2,6 @@ package com.fundly.user.model;
 
 
 import com.persistence.dto.LikeDto;
-import com.persistence.dto.UserDto;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
@@ -12,22 +11,17 @@ import java.util.Map;
 @Mapper
 public interface LikeDao {
     //좋아요 목록 확인
-//    List<LikeDto> getLikeList(LikeDto likedto) throws Exception;
+    List<LikeDto> getLikeList(LikeDto likedto) throws Exception;
 
     //처음 좋아요
     int insertLike(LikeDto likedto) throws Exception;
 
     //좋아요 취소
-//    int cancelLike(LikeDto likedto) throws Exception;
-//
-//    //다시 좋아요
-//    void reLike(LikeDto likedto) throws Exception;
-//
-//    //전체목록 삭제
-//    void deleteAllLike(LikeDto likedto) throws Exception;
-//
-//    //이미 좋아요한 프로젝트인지 조회
-////    LikeDto checkLike(@Param("likedto") LikeDto likedto);
-//
-//    LikeDto checkLike(String userId, String pjId);
+    int cancelLike(LikeDto likedto) throws Exception;
+
+    //다시 좋아요
+    int reLike(LikeDto likedto) throws Exception;
+
+    //전체목록 삭제
+    int deleteAllLike() throws Exception;
 }
