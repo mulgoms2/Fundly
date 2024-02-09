3e922bba1e88b9f2ff74ad0156b536fb73522235
pickylemon <novecento104@gmail.com>
2024-02-09 10:27:53 +0900

    new: gift register - reward.jsp

--- src/main/java/config/ServletContext.java
+++ src/main/java/config/ServletContext.java
@@ -31,7 +31,7 @@ public class ServletContext implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**").addResourceLocations("/WEB-INF/static/");
-    }
+       }
 
     @Bean
     public CommonsMultipartResolver commonsMultipartResolver() {
