40f3a58159a95363d01f7c72a522dd19ad1c62c7
yoonjae <mulgoms2@gmail.com>
2024-02-10 18:05:22 +0900

    remove: testFile

--- src/main/java/com/fundly/chat/pojo/HttpHandShakeInterceptor.java
+++ src/main/java/com/fundly/chat/pojo/HttpHandShakeInterceptor.java
@@ -23,6 +23,5 @@ public class HttpHandShakeInterceptor implements HandshakeInterceptor {
     }
     @Override
     public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Exception exception) {
-
     }
 }
