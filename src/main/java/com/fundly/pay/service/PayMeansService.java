8f0d121d1ab28d1dd20e1376208f39ab66ba9575
howecofe <am0229@naver.com>
2024-02-13 19:53:13 +0900

    update: '기본 결제수단 지정' 관련 메서드 추가 및 파라미터 수정

--- src/main/java/com/fundly/pay/service/PayMeansService.java
+++ src/main/java/com/fundly/pay/service/PayMeansService.java
@@ -4,19 +4,20 @@ import com.persistence.dto.PayMeansDto;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 
 @Service
 public interface PayMeansService {
     int getPayMeansCount() throws Exception;
-    String getPayMeansId(String UserId) throws Exception;
+    String getPayMeansId(String user_id) throws Exception;
     int registerPayMeans(PayMeansDto payMeansDto) throws Exception;
-    int unsetDefaultPayMeans(PayMeansDto payMeansDto) throws Exception;
+    int unsetDefaultPayMeans(Map map) throws Exception;
     int setDefaultPayMeans(PayMeansDto payMeansDto) throws Exception;
     int removePayMeans(String pay_means_id) throws Exception;
     int removeAllPayMeansForAdmin() throws Exception;
     PayMeansDto getPayMeans(String pay_means_id) throws Exception;
     List<PayMeansDto> getAllPayMeans(String user_id) throws Exception;
     List<PayMeansDto> getAllPayMeansForAdmin() throws Exception;
-    PayMeansDto getDefaultPayMeans() throws Exception;
-
+    PayMeansDto getDefaultPayMeans(String user_id) throws Exception;
+    int getDefaultPayMeansCount(String user_id) throws Exception;
 }
