f5d88d2c881e25bfe15099046a4bf2c6e76f8831
bada <silverzoo030@gmail.com>
2024-02-13 16:58:47 +0900

    ⚡️ Ing: modify send likeInfo from detail page to Controller

--- src/main/java/com/fundly/user/controller/LikeController.java
+++ src/main/java/com/fundly/user/controller/LikeController.java
@@ -26,42 +26,8 @@ public class LikeController {
     @Autowired
     LikeService likeservice;
 
-    @RequestMapping(value="/like")
-    public String like(HttpServletRequest req, Model model) throws Exception {
-        String pj_id = req.getParameter("pj_id");
-        String user_id = req.getParameter("user_id");
-
-        LikeDto likedto = new LikeDto();
-        likedto.setPj_id(pj_id);
-        likedto.setUser_id(user_id);
-
-        model.addAttribute("pj_id", pj_id);
-        model.addAttribute("user_id", user_id);
-        model.addAttribute("like_status", likeservice.checkLike(likedto));
-        model.addAttribute("list", likeservice.getList(likedto));
-        return "user/like";
-    }
-
-    @PostMapping("/like2")
-    @ResponseBody
-    public List<LikeDto> addLikePj(HttpServletRequest req, Model model) throws Exception {
-        String pj_id = req.getParameter("pj_id");
-        String user_id = req.getParameter("user_id");
-
-        LikeDto likedto = new LikeDto();
-        likedto.setPj_id(pj_id);
-        likedto.setUser_id(user_id);
-
-        model.addAttribute("pj_id", pj_id);
-        model.addAttribute("user_id", user_id);
-        model.addAttribute("like_status", likeservice.checkLike(likedto));
-        model.addAttribute("list", likeservice.getList(likedto));
-        return likeservice.getList(likedto);
-    }
-
-//    @GetMapping("/detail")
-//    @ResponseBody
-//    public ResponseEntity<List<LikeDto>> addLikePj(HttpServletRequest req, Model model) throws Exception {
+//    @RequestMapping(value="/like")
+//    public String like(HttpServletRequest req, Model model) throws Exception {
 //        String pj_id = req.getParameter("pj_id");
 //        String user_id = req.getParameter("user_id");
 //
@@ -73,29 +39,15 @@ public class LikeController {
 //        model.addAttribute("user_id", user_id);
 //        model.addAttribute("like_status", likeservice.checkLike(likedto));
 //        model.addAttribute("list", likeservice.getList(likedto));
-//        List<LikeDto> likes = likeservice.getList(likedto);
-//        return new ResponseEntity<>(likes, HttpStatus.OK);
+//        return "user/like";
 //    }
 
-//    @DeleteMapping("/item")
-//    @ResponseBody
-//    public ResponseEntity<List<ItemDto>> removeItem(String item_id, HttpSession session){
-////        System.out.println("itemDto = " + itemDto);
-////        System.out.println("itemDto.getItem_id() = " + itemDto.getItem_id());
-//
-//        //아이디가 일치해야만 아이템 삭제가 가능하도록
-//        //String id = (String)session.getAttribute("id"); 원래는 session으로부터 로그인 아이디를 얻어와야함
-//        String id = "asdf";
-//        String pj_id = "pj1";
-//        try {
-//            int rowCnt = itemService.remove(item_id, id);
-//            System.out.println("rowCnt = " + rowCnt);
-//            if(rowCnt!=1) throw new Exception("item delete ERR");
-//            List<ItemDto> list = itemService.getItemList(pj_id);
-//            return new ResponseEntity<>(list, HttpStatus.OK);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-//        }
-//    }
+    @PostMapping("/like2")
+    @ResponseBody
+    public List<LikeDto> addLikePj(@RequestBody LikeDto likedto) throws Exception {
+        // 좋아요 상태 확인
+        int isLike =  likeservice.checkLike(likedto);
+        likedto.setLike_status(isLike);
+        return likeservice.getList(likedto);
+    }
 }
--- src/main/webapp/WEB-INF/static/product/like.js
+++ src/main/webapp/WEB-INF/static/product/like.js
@@ -1,115 +1,80 @@
-// /* JSP SCRIPT */
-// var bbsidx = ${bbsidx};
-// var useridx = ${useridx};
-//
-// var likeBtn = document.getElementById("likeBtn");
-// likeBtn.onclick = function(){ changeHeart(); }
-//
-// /* 좋아요 버튼 눌렀을떄 */
-// function changeHeart(){
-//     $.ajax({
-//         type : "POST",
-//         url : "/clickLike",
-//         dataType : "json",
-//         data : "bbsidx="+bbsidx+"&useridx="+useridx,
-//         error : function(){
-//             Rnd.alert("통신 에러","error","확인",function(){});
-//         },
-//         success : function(jdata) {
-//             if(jdata.resultCode == -1){
-//                 Rnd.alert("좋아요 오류","error","확인",function(){});
-//             }
-//             else{
-//                 if(jdata.likecheck == 1){
-//                     $("#likeBtn").attr("src","/home/img/ico_like_after.png");
-//                     $("#likecnt").empty();
-//                     $("#likecnt").append(jdata.likecnt);
-//                 }
-//                 else if (jdata.likecheck == 0){
-//                     $("#likeBtn").attr("src","/home/img/ico_like_before.png");
-//                     $("#likecnt").empty();
-//                     $("#likecnt").append(jdata.likecnt);
-//
-//                 }
-//             }
-//         }
-//     });
-// }
-//
-// // $(document).ready(function() {
-// //        $(".like").on("click", function(){
-// //
-// //            $.ajax({
-// //                    url : "detail.jsp",
-// //                    type: 'POST',
-// //                    data: {'pj_id':'${pj_id}', 'buyer_id':'${user_id}'},
-// //            success:function(data){
-// //
-// //                if(data==1){
-// //                    like2 = true;
-// //                    alert("상품 찜 하셨습니다.");
-// //                    $('#like').attr("src","빨간하트");
-// //                    var result = confirm('찜목록으로 이동하시겠습니까?');
-// //                    if (result) {
-// //                        //yes
-// //                        //찜 리스트 페이지 생성 후 -> 찜리스트 페이지 이동으로 변경
-// //                        location.href='/user/like';
-// //
-// //                    }
-// //
-// //                }
-// //                else if(data == -1){
-// //                    alert("로그인이 필요한 서비스입니다. ");
-// //
-// //
-// //                }
-// //                else {
-// //                    like2 =false;
-// //                    alert("상품 찜 취소하셨습니다. ");
-// //                    $('#like').attr("src","빈하트");
-// //                }
-// //
-// //            },
-// //            error:function(error){
-// //                console.log(error);
-// //            }
-// //
-// //
-// // 		 	});
-// //        });
-// //    });
-
 // 좋아요 버튼 클릭시 좋아요 + -
+
+const clickLikeBtn = () => {
+    const obj = {
+      pj_id : "P1010",
+      user_id : "user"
+    };
+
+//     if 버튼이 안눌린상태 일때
+    fetch("/like2", {
+        method: "POST",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify(obj)
+    }).then(res => res.json()).then(handleLike);
+
+
+    // if ()
+//     1. 서버로 좋아요 정보 전송
+//     2. 서버 응답 받는다.
+//     3. 응담 성공시 좋아요 카운트 증가
+//     4. 좋아요 버튼 색상 변경
+
+//     if 버튼이 눌려있는 상태일떄
+//     위와 반대의 동작
+};
+
+const handleLike = (likeDto) => {
+    console.log(likeDto[0].like_status);
+}
+
+const fetchLikeInfo = () => {
+    var params = $('#formtest').serialize();
+
+    return fetch("/chat", {
+        method: "POST",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: params
+    });
+}
+
 function clicklike(type) {
     const like_cnt = document.getElementById("like_cnt")
     let curr_cnt = like_cnt.innerText;
 
-    if(type === 'plus') {
+    if (type === 'plus') {
         curr_cnt = parseInt(curr_cnt) + 1;
         alert("좋아요")
-    } else if(type === 'minus') {
-        curr_cnt = parseInt(curr_cnt) -1 ;
+    } else if (type === 'minus') {
+        curr_cnt = parseInt(curr_cnt) - 1;
         alert("좋아요취소")
     }
 
     like_cnt.innerText = curr_cnt;
+
+    sendLikeInfo();
+
 }
 
 // ajax로 좋아요 클릭시 데이터 전송 (페이지 이동x)
 
-function sendajax(){
+function sendLikeInfo() {
 
     var params = $('#formtest').serialize();
 
     $.ajax({
         url: "/like2",
-        dataType : "json",
+        dataType: "json",
         data: params,
         type: "POST",
-        success : function(){
+        success: function (data) {
             alert("성공")
         },
-        error : function(){
+        error: function () {
             alert("에러")
         }
     });
