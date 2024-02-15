3c07f96f185d50b737a717c975602e6523c86710
taemin444 <fresh1115@naver.com>
2024-02-15 15:46:22 +0900

    fix : exceptionHandler 경로수정

--- src/main/java/com/fundly/admin/controller/ControllerExceptionHandler.java
+++ src/main/java/com/fundly/admin/controller/ControllerExceptionHandler.java
@@ -1,15 +1,17 @@
 package com.fundly.admin.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.sql.SQLException;
 
+@Slf4j
 @ControllerAdvice("com.fundly.admin")
 public class ControllerExceptionHandler {
     @ExceptionHandler(RuntimeException.class)
     public String handleRuntimeException() {
-
+        log.error("adminExceptionHandler");
         return "admin/error";
     }
 }
