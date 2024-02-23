38f83be05804b376c7be5b543b0a4d0737212f7d
howecofe <am0229@naver.com>
2024-02-23 19:24:29 +0900

    new: 결제 DTO & MyBatis Mapper & DAO 작성

---
+++ src/main/java/com/fundly/pay/model/PayDao.java
@@ -0,0 +1,20 @@
+package com.fundly.pay.model;
+
+import com.persistence.dto.PayDto;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository
+public interface PayDao {
+    PayDto selectCompletedOrder() throws Exception;
+    String selectPayId(String userId) throws Exception;
+    int insert(PayDto payDto) throws Exception;
+    PayDto selectPayTarget() throws Exception;
+    PayDto selectPayRetryTarget() throws Exception;
+    int updatePayStatusToCompleted(PayDto payDto) throws Exception;
+    int updatePayStatusToFailed(PayDto payDto) throws Exception;
+    int updatePayStatusToRetryFailed(PayDto payDto) throws Exception;
+    PayDto selectPayForUser(String userId) throws Exception;
+    PayDto selectPayForProject(String pjId) throws Exception;
+}
