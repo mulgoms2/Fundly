4e652a475f634e5f58b39cb8e7a1e97ab6e18264
howecofe <am0229@naver.com>
2024-02-23 19:29:33 +0900

    update: OrderDto 상품전달에정일 변수 삭제

--- src/main/java/com/persistence/dto/OrderDto.java
+++ src/main/java/com/persistence/dto/OrderDto.java
@@ -26,7 +26,6 @@ public class OrderDto {
     private String cancel_reason;
     private Date order_ddln_dtm;
     private Date pay_due_dtm;
-    private Date gift_ship_due_date; // TODO: 주문마감일시 : 추가되어야 할 컬럼. (프로젝트 테이블에 존재) (update 24/02/23)
     private Date dba_reg_dtm;
     private String dba_reg_id;
     private Date dba_mod_dtm;
