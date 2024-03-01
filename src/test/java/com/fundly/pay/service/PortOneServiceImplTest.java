5788479687fd31e4a96c9f439a7f572e00248213
howecofe <am0229@naver.com>
2024-03-01 21:39:51 +0900

    update: PortOneServiceImpl 리팩토링 및 테스트코드 작성

--- src/test/java/com/fundly/pay/service/PortOneServiceImplTest.java
+++ src/test/java/com/fundly/pay/service/PortOneServiceImplTest.java
@@ -2,13 +2,17 @@ package com.fundly.pay.service;
 
 import com.fundly.pay.dto.billkey.BillKeyRequestDto;
 import com.fundly.pay.dto.billkey.BillKeyResponseDto;
+import com.fundly.pay.dto.payment.PaymentRequestDto;
+import com.fundly.pay.dto.payment.PaymentResponseDto;
 import com.fundly.pay.dto.schedule.ScheduledPayRequestDto;
 import com.fundly.pay.dto.schedule.ScheduledPayResponseDto;
+import com.fundly.pay.dto.token.TokenResponseDto;
 import com.fundly.pay.model.PayMeansDao;
 import config.RootContext;
 import config.ServletContext;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,8 +20,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 
+import java.math.BigInteger;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Slf4j
 @SpringJUnitWebConfig({RootContext.class, ServletContext.class})
@@ -41,86 +46,114 @@ class PortOneServiceImplTest {
     @Value("${PAY_PWD_2DIGIT}")
     private String PAY_PWD_2DIGIT;
 
-    @Test
+    String authToken = "";
+    String userId = "test";
+    String payMeansId = "";
+    PaymentRequestDto paymentRequestDto;
+    BillKeyRequestDto billKeyRequestDto;
+
+    @BeforeEach
     @SneakyThrows
+    @DisplayName("테스트 수행 전 초기화")
+    void init() {
+        payMeansId = payMeansDao.selectPayMeansId(userId);
+
+        // 빌링키 발급 & 삭제 RequestDto 생성
+        billKeyRequestDto = BillKeyRequestDto.builder()
+                .cardNo(PAY_CARD_NO)
+                .expiry(PAY_EXPIRY)
+                .pg("nice_v2")
+                .birth(PAY_BIRTH)
+                .pwd2digit(PAY_PWD_2DIGIT)
+                .customer_uid(payMeansId)
+                .build();
+
+        // 결제 & 결제취소 RequestDto 생성
+        paymentRequestDto = PaymentRequestDto.builder()
+                .customer_uid(payMeansId)
+                .amount(BigInteger.valueOf(100))
+                .name("test")
+                .build();
+
+        // PortOne 인증 토큰 발급
+        ResponseEntity<TokenResponseDto> tokenResponseDto = portOneService.getToken();
+        assertEquals(tokenResponseDto.getStatusCodeValue(), 200);
+        assertEquals(tokenResponseDto.getBody().getCode(), 0);
+        authToken = tokenResponseDto.getBody().getResponse().getAccess_token();
+
+        // 빌링키 발급
+        ResponseEntity<BillKeyResponseDto> billKeyResponseDto = portOneService.getBillKey(billKeyRequestDto, authToken);
+        assertEquals(billKeyResponseDto.getStatusCodeValue(), 200);
+        assertEquals(billKeyResponseDto.getBody().getCode(), 0);
+    }
+
+    @Test
+    @DisplayName("결제")
+    void requestPayTest() {
+        paymentRequestDto.setMerchant_uid("requestPayTest_99999");
+
+        ResponseEntity<PaymentResponseDto> paymentResponseDto = portOneService.requestPay(paymentRequestDto, authToken);
+        assertEquals(paymentResponseDto.getStatusCodeValue(), 200);
+        assertEquals(paymentResponseDto.getBody().getCode(), 0);
+    }
+
+    @Test
+    @DisplayName("결제 취소")
+    void cancelPayTest() {
+        paymentRequestDto.setMerchant_uid("cancelPayTest_99999");
+
+        // 결제
+        ResponseEntity<PaymentResponseDto> paymentResponseDto = portOneService.requestPay(paymentRequestDto, authToken);
+        assertEquals(paymentResponseDto.getStatusCodeValue(), 200);
+        assertEquals(paymentResponseDto.getBody().getCode(), 0);
+
+        // 결제 취소
+        paymentResponseDto = portOneService.cancelPay(paymentRequestDto, authToken);
+        assertEquals(paymentResponseDto.getStatusCodeValue(), 200);
+        assertEquals(paymentResponseDto.getBody().getCode(), 0);
+    }
+
+    @Test
     @DisplayName("빌링키 삭제")
     void removeBillKeyTest() {
-        assertEquals(portOneService.getToken().getBody().getCode(), 0, "[removeBillKeyTest] getToken 테스트 실패");
-        String authToken = portOneService.getToken().getBody().getResponse().getAccess_token();
-
-        String userId = "test";
-        String payMeansId = payMeansDao.selectPayMeansId(userId);
-        BillKeyRequestDto billKeyRequestDto = new BillKeyRequestDto(PAY_CARD_NO, PAY_EXPIRY, PAY_BIRTH, PAY_PWD_2DIGIT, payMeansId);
-        ResponseEntity<BillKeyResponseDto> billKeyResponseDto = portOneService.getBillKey(billKeyRequestDto, authToken);
-        assertEquals(billKeyResponseDto.getBody().getCode(), 0, "[removeBillKeyTest] getBillKey 테스트 실패");
-
-        // 결제예약내역이 없는지 확인 후 삭제
-        long currentUnixTime = System.currentTimeMillis() / 1000;
-        long threeMonthsAgoUnixTime = currentUnixTime - (3L * 30L * 24L * 60L * 60L);
-        ScheduledPayRequestDto scheduledPayRequestDto = new ScheduledPayRequestDto(billKeyRequestDto.getCustomer_uid(), "scheduled", threeMonthsAgoUnixTime, currentUnixTime);
-        ResponseEntity<ScheduledPayResponseDto> scheduledPayResponseDto = portOneService.getScheduledPay(scheduledPayRequestDto, authToken);
-        assertEquals(scheduledPayResponseDto.getBody().getCode(), 0, "[removeBillKeyTest] getScheduledPay 테스트 실패");
-        assertEquals(scheduledPayResponseDto.getBody().getResponse().getTotal(), 0, "[removeBillKeyTest] getScheduledPay Total 테스트 실패");
-
-        billKeyResponseDto = portOneService.removeBillKey(billKeyRequestDto, authToken);
-        assertEquals(billKeyResponseDto.getBody().getCode(), 0, "[removeBillKeyTest] removeBillKeyTest 테스트 실패");
+        // 빌링키 삭제
+        ResponseEntity<BillKeyResponseDto> billKeyResponseDto = portOneService.removeBillKey(billKeyRequestDto, authToken);
+        assertEquals(billKeyResponseDto.getStatusCodeValue(), 200);
+        assertEquals(billKeyResponseDto.getBody().getCode(), 0);
     }
 
     @Test
-    @SneakyThrows
-    @DisplayName("결제 예약 내역 조회")
+    @DisplayName("예약된 결제 내역 조회")
     void getScheduledPayTest() {
-        // TODO: 결제예약요청 메서드 만들어지면 total 카운트로 검증해보기
+        long currentUnixTime = System.currentTimeMillis() / 1000; // 현재 UTC Time
+        long threeMonthsAgoUnixTime = currentUnixTime - (3L * 30L * 24L * 60L * 60L); // 3개월 전 UTC Time
+        ScheduledPayRequestDto scheduledPayRequestDto = ScheduledPayRequestDto.builder()
+                .customer_uid(payMeansId)
+                .schedule_status("scheduled")
+                .from(threeMonthsAgoUnixTime)
+                .to(currentUnixTime)
+                .build();
 
-        assertEquals(portOneService.getToken().getBody().getCode(), 0,  "[getScheduledPayTest] getToken 테스트 실패");
-        String authToken = portOneService.getToken().getBody().getResponse().getAccess_token();
-
-        String userId = "test";
-        String payMeansId = payMeansDao.selectPayMeansId(userId);
-        BillKeyRequestDto billKeyRequestDto = new BillKeyRequestDto(PAY_CARD_NO, PAY_EXPIRY, PAY_BIRTH, PAY_PWD_2DIGIT, payMeansId);
-        ResponseEntity<BillKeyResponseDto> billKeyResponseDto = portOneService.getBillKey(billKeyRequestDto, authToken);
-        assertEquals(billKeyResponseDto.getBody().getCode(), 0, "[getScheduledPayTest] getBillKey 테스트 실패");
-
-        long currentUnixTime = System.currentTimeMillis() / 1000;
-        long threeMonthsAgoUnixTime = currentUnixTime - (3L * 30L * 24L * 60L * 60L);
-        ScheduledPayRequestDto scheduledPayRequestDto = new ScheduledPayRequestDto(billKeyRequestDto.getCustomer_uid(), "scheduled", threeMonthsAgoUnixTime, currentUnixTime);
+        // 예약된 결제 내역 조회
         ResponseEntity<ScheduledPayResponseDto> scheduledPayResponseDto = portOneService.getScheduledPay(scheduledPayRequestDto, authToken);
-        assertEquals(scheduledPayResponseDto.getBody().getCode(), 0, "[getScheduledPayTest] getScheduledPay 테스트 실패");
+        assertEquals(scheduledPayResponseDto.getStatusCodeValue(), 200);
+        assertEquals(scheduledPayResponseDto.getBody().getCode(), 0);
+        assertEquals(scheduledPayResponseDto.getBody().getResponse().getTotal(), 0); // 예약된 결제 내역 0건
     }
 
-    @Test
-    @SneakyThrows
-    @DisplayName("빌링키 발급")
-    void getBillKeyTest() {
-        assertEquals(portOneService.getToken().getBody().getCode(), 0, "[getBillKeyTest] getToken 테스트 실패");
-        String authToken = portOneService.getToken().getBody().getResponse().getAccess_token();
+//    @Test
+//    @DisplayName("빌링키 발급")
+//    void getBillKeyTest() {
+//        ResponseEntity<BillKeyResponseDto> billKeyResponseDto = portOneService.getBillKey(billKeyRequestDto, authToken);
+//        assertEquals(billKeyResponseDto.getStatusCodeValue(), 200);
+//        assertEquals(billKeyResponseDto.getBody().getCode(), 0);
+//    }
 
-        String userId = "test";
-        String payMeansId = payMeansDao.selectPayMeansId(userId);
-        BillKeyRequestDto billKeyRequestDto = new BillKeyRequestDto("1111111111111111", "2027-04", "970101", "11", payMeansId);
-        ResponseEntity<BillKeyResponseDto> billKeyResponseDto = portOneService.getBillKey(billKeyRequestDto, authToken);
-        assertTrue(billKeyResponseDto.getBody().getCode() != 0);
-
-        billKeyRequestDto = new BillKeyRequestDto(PAY_CARD_NO, PAY_EXPIRY, PAY_BIRTH, PAY_PWD_2DIGIT, payMeansId);
-        billKeyResponseDto = portOneService.getBillKey(billKeyRequestDto, authToken);
-        assertEquals(billKeyResponseDto.getBody().getCode(), 0, "[getBillKeyTest] getBillKey 테스트 실패");
-    }
-
-    @Test
-    @SneakyThrows
-    @DisplayName("인증 토큰 발급")
-    void getTokenTest() {
-        assertEquals(portOneService.getToken().getStatusCodeValue(), 200, "[getTokenTest] getToken 테스트 실패");
-        assertEquals(portOneService.getToken().getBody().getCode(), 0, "[getTokenTest] getToken 테스트 실패");
-
-        // Exception 처리 확인
-//        try {
-//            log.info("portOneService.getToken().getStatusCode(): " + portOneService.getToken().getStatusCode());
-//            log.info("portOneService.getToken().getStatusCodeValue(): " + portOneService.getToken().getStatusCodeValue());
-//            log.info("portOneService.getToken() authToken: " + Objects.requireNonNull(portOneService.getToken().getBody()).getResponse().getAccess_token());
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            log.info("에러가 발생했어요 !!!" + e.getMessage());
-//        }
-    }
+//    @Test
+//    @DisplayName("인증 토큰 발급")
+//    void getTokenTest() {
+//        ResponseEntity<TokenResponseDto> tokenResponseDto = portOneService.getToken();
+//        assertEquals(tokenResponseDto.getStatusCodeValue(), 200);
+//        assertEquals(tokenResponseDto.getBody().getCode(), 0);
+//    }
 }
