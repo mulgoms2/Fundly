904299c1b1413fc9c5ec64d8ce93911473dff5bf
howecofe <am0229@naver.com>
2024-03-01 21:19:29 +0900

    update: ScheduledPayRequestDto Builder 어노테이션 추가

--- src/main/java/com/fundly/pay/dto/schedule/ScheduledPayRequestDto.java
+++ src/main/java/com/fundly/pay/dto/schedule/ScheduledPayRequestDto.java
@@ -1,14 +1,12 @@
 package com.fundly.pay.dto.schedule;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @ToString
+@Builder
 public class ScheduledPayRequestDto {
     private String customer_uid; // pay_means_id
     private String schedule_status; // scheduled: 예약, executed: 예약결제완료, revoked: 예약취소
