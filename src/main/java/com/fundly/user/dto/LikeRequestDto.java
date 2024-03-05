7f1ffdc8260de2898f2f54b1310894de34edd668
bada <silverzoo030@gmail.com>
2024-03-05 13:30:36 +0900

    ✨ New: requestDto for like feature

--- src/main/java/com/fundly/user/dto/LikeRequestDto.java
+++ src/main/java/com/fundly/user/dto/LikeRequestDto.java
@@ -1,2 +1,16 @@
-package com.fundly.user.dto;public class LikeRequestDto {
+package com.fundly.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LikeRequestDto {
+    private String user_id;
+    private String pj_id;
+    private int curr_pj_like_cnt;
 }
