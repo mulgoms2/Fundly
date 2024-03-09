f43d7b09bea8415cc2896f58d5c954d396d093cd
yoonjae <mulgoms2@gmail.com>
2024-03-09 20:37:34 +0900

    프로젝트 기본정보 탭 대표이미지 업로드기능 추가

--- src/main/webapp/WEB-INF/static/project/js/creator.js
+++ src/main/webapp/WEB-INF/static/project/js/creator.js
@@ -1,5 +1,20 @@
-const saveProject = async (e) => {
-//     버튼이 눌리면 프로젝트 변경 사항을 서버에 보낸다.
+const initHandler = () => {
+    const saveBtn = document.getElementById("saveBtn");
+    saveBtn.addEventListener("click", handleSaveBtnClick);
+
+    const imgInput = document.getElementById("pjImgUp");
+    imgInput.addEventListener("input", handleImgInput);
+};
+
+const handleSaveBtnClick = () => {
+    //     버튼이 눌리면 프로젝트 변경 사항을 서버에 보낸다.
+    const endPoint = "/project/editor/creator";
+    const formData = getCreatorInfoFormData();
+
+    postProject(endPoint, formData);
+}
+
+const getCreatorInfoFormData = () => {
     const formData = new FormData();
     const sel_name = document.getElementById("selName").value;
     const sel_intro = document.getElementById("selIntro").value;
@@ -7,48 +22,16 @@ const saveProject = async (e) => {
     formData.append("pj_sel_name", sel_name);
     formData.append("pj_sel_short_intro", sel_intro);
 
-    const response = await fetch("/project/editor/creator",{
-        method: "post",
-        headers: {},
-        body: formData,
-    });
-
-    const isSucceed = await response.json();
-
-    if (isSucceed)
-        alert("저장이 완료되었습니다.")
-};
-
-const handleImgInput = (e) => {
-    uploadProfileImg();
-    e.target.value = "";
-};
-
-const uploadProfileImg = async () => {
-    const endPoint = "/project/editor/creator/image";
-
-    const response = await fetch(endPoint, {
-        method : "post",
-        headers:{},
-        body: getImageFormData("pjImgUp"),
-    });
-
-    const saved_url = await response.text();
-
-    printImgTag("profImg", saved_url);
-};
-
-const printImgTag = (tagId, imgSrc) => {
-    document.getElementById(tagId).src = imgSrc;
-};
-const uploadAccountImage = () => {
-};
-
-const getImageFormData = (tagId) => {
-    const formData = new FormData();
-    const imgFile = document.getElementById("pjImgUp").files[0];
-
-    formData.append("image", imgFile);
-
     return formData;
 };
+
+const handleImgInput = async (e) => {
+    const endPoint = "/project/editor/creator/image";
+    const imgFormData = getImageFormData("pjImgUp");
+    const src_url = await fetchImage(endPoint, imgFormData);
+
+    printImgTag("profImg", src_url);
+    clearInput(e);
+};
+
+initHandler();
