006afb631dc44bb09264edca6d18b03f7a50daaf
bada <silverzoo030@gmail.com>
2024-03-11 20:49:09 +0900

    ✨ New: new dto for orderdetail

--- src/main/java/com/persistence/dto/OrderGiftDetailsDto.java
+++ src/main/java/com/persistence/dto/OrderGiftDetailsDto.java
@@ -1,2 +1,28 @@
-package com.persistence.dto;public class OrderGiftDetailsDto {
+package com.persistence.dto;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class OrderGiftDetailsDto {
+    private String order_detail_seq;
+    private String order_no;
+    private String gift_id;
+    private String order_gift_name;
+    private int order_gift_qty;
+
+    // DBA
+    @EqualsAndHashCode.Exclude
+    private LocalDateTime dba_reg_dtm; // 최초등록일시
+    private String dba_reg_id; // 최초등록자식별번호
+    @EqualsAndHashCode.Exclude
+    private LocalDateTime dba_mod_dtm; // 최종수정일시
+    private String dba_mod_id; // 최종수정자식별번호
 }
