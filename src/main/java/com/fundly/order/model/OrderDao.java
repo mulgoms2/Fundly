29e79dc81f474e29520b84b648c6b5c4d0006db3
bada <silverzoo030@gmail.com>
2024-03-15 00:43:52 +0900

    ✅ Ing: order feature

--- src/main/java/com/fundly/order/model/OrderDao.java
+++ src/main/java/com/fundly/order/model/OrderDao.java
@@ -1,7 +1,13 @@
 package com.fundly.order.model;
 
+import com.persistence.dto.OrderDto;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.dao.DataAccessException;
 
 @Mapper
 public interface OrderDao {
+
+    // 상세페이지에서 insert
+    // 주문페이지에서 select
+    OrderDto select(String order_list_id) throws DataAccessException;
 }
--- src/main/java/com/fundly/order/model/OrderGiftDetailsDao.java
+++ src/main/java/com/fundly/order/model/OrderGiftDetailsDao.java
@@ -1,7 +1,14 @@
 package com.fundly.order.model;
 
-public class OrderGiftDetailsDao {
+import com.persistence.dto.OrderGiftDetailsDto;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.dao.DataAccessException;
+
+@Mapper
+public interface OrderGiftDetailsDao {
 
     // 상세페이지에서 insert
+
     // 주문페이지에서 select
+    OrderGiftDetailsDto select(String order_no) throws DataAccessException;
 }
